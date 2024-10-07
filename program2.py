8fa14caa3b5aa72dbaec7ed987ee434d3a9fb67b
Dev-Sneh <krishagrawal343@gmail.com>
2024-10-07 12:24:07 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -3,7 +3,8 @@ def decode_message( s: str, p: str) -> bool:
         dp=[[false]*(n+1) for _ in range(m+1)]
         dp[0][0]=True
         for j in range(1,n+1):
-                if p[j-1]=='*'
+                if p[j-1]=='*':
+                        dp[0][j]=dp[0][j-1]
         
         
 
