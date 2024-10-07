7b8fa2d70fb4cc2e2a9b70d84d6d55cb8f6ff1e8
Dev-Sneh <krishagrawal343@gmail.com>
2024-10-07 12:27:50 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,11 +1,13 @@
-def decode_message( s: str, p: str) -> bool:
-        m,n = len(s), len(p)
-        dp=[[false]*(n+1) for _ in range(m+1)]
-        dp[0][0]=True
-        for j in range(1,n+1):
-                if p[j-1]=='*':
-                        dp[0][j]=dp[0][j-1]
-        for i in range(1, m + 1):
+def decode_message(s: str, p: str) -> bool:
+    m, n = len(s), len(p)
+    dp = [[False] * (n + 1) for _ in range(m + 1)]
+    dp[0][0] = True
+
+    for j in range(1, n + 1):
+        if p[j - 1] == '*':
+            dp[0][j] = dp[0][j - 1]
+    
+    for i in range(1, m + 1):
         for j in range(1, n + 1):
             if p[j - 1] == s[i - 1] or p[j - 1] == '?':
                 dp[i][j] = dp[i - 1][j - 1]
@@ -14,11 +16,7 @@ def decode_message( s: str, p: str) -> bool:
     
     return dp[m][n]
 
-                        
-                   
-        
-        
-
-
-  
-        return False
+print(decode_message("aa", "a"))      
+print(decode_message("aa", "*"))      
+print(decode_message("cb", "?a"))     
+print(decode_message("adceb", "*a*b")) 
