33224ede87b5e45c69b93c71eb33d821160477df
Dev-Sneh <krishagrawal343@gmail.com>
2024-10-07 12:25:45 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -10,6 +10,7 @@ for i in range(1, m + 1):
                 if p[j - 1] == s[i - 1] or p[j - 1] == '?':
                          dp[i][j] = dp[i - 1][j - 1]
                 elif p[j - 1] == '*':
+                        dp[i][j] = dp[i - 1][j] or dp[i][j - 1]
         
         
 
